e71971a7ee32df3a032106c85b3e048842ecc960
59114_Chetana <deslechetna1999@gmail.com>
2022-06-16 07:17:05 +0530

    [Chetana] Added : Wite Function

--- EmpWage.js
+++ EmpWage.js
@@ -1,4 +1,3 @@
-//UC1 IF ELSE CONDITION
 {
     const IS_ABSENT= 0
     let empCheck = Math.floor(Math.random() * 10) % 2;
@@ -11,27 +10,27 @@
         console.log(" EMPLOYEE is Present ");
     }
 }
-
-//UC2 SWITCH CASE
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOUR=8;
-const WAGE_PER_HOUR=20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
 {
-    let empHrs =  0;
-    let empCheck= Math.floor(Math.random() * 10) % 3;
-    switch(empCheck)
+    function getWorkingHours(empCheck)
     {
-        case IS_PART_TIME:
-            empHrs=PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            empHrs=FULL_TIME_HOUR;
-            break;
-        default:
-            empHrs=0;
+        switch(empCheck)
+        {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOUR;
+            default:
+                return 0;
+        }
     }
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
     let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("Emp Wage: " + empWage);
+    console.log("Employee Wage: " + empWage);
+    console.log("Hours :"+empHrs);
 }
